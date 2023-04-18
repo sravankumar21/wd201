b6e27f721219b2b735af031ecdd97605cb1bfb80
Sravan Kumar <102661231+sravankumar21@users.noreply.github.com>
2023-04-18 09:40:50 -0700

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,73 +1,20 @@
+// __tests__/todo.js
 /* eslint-disable no-undef */
-const todoList = require("../todo");
-let today = new Date().toLocaleDateString("en-CA");
+const db = require("../models");
 
-const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
+describe("Todolist Test Suite", () => {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+  });
 
-describe("Todolist Testing", () => {
-  beforeAll(() => {
-    add({
-      title: "DAA algorithums",
+  test("Should add new todo", async () => {
+    const todoItemsCount = await db.Todo.count();
+    await db.Todo.addTask({
+      title: "Test todo",
       completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
+      dueDate: new Date(),
     });
-  });
-
-  test("Add a new todo in list", () => {
-    // expect(all.length).toBe(0);
-
-    let length = all.length;
-
-    add({
-      title: "node js learning",
-      completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
-    });
-
-    expect(all.length).toBe(length + 1);
-  });
-
-  test("Mark todo as a completed", () => {
-    expect(all[0].completed).toBe(false);
-    markAsComplete(0);
-    expect(all[0].completed).toBe(true);
-  });
-
-  test("retrive all todos that are overdue", () => {
-    let listOfTodos = overdue();
-
-    expect(
-      listOfTodos.every((todo) => {
-        return todo.dueDate < today;
-      })
-    ).toBe(true);
-  });
-
-  test("retrive all todos that are dueToday", () => {
-    let listOfTodos = dueToday();
-
-    expect(
-      listOfTodos.every((todo) => {
-        return todo.dueDate === today;
-      })
-    ).toBe(true);
-  });
-
-  test("retrive all todos that are dueLater", () => {
-    let listOfTodos = dueLater();
-
-    expect(
-      listOfTodos.every((todo) => {
-        return todo.dueDate > today;
-      })
-    ).toBe(true);
+    const newTodoItemsCount = await db.Todo.count();
+    expect(newTodoItemsCount).toBe(todoItemsCount + 1);
   });
 });
-
-
-
-
-
-
-
-
